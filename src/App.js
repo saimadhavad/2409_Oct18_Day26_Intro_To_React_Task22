6e40acc89679abd71be1cb34136f3a2560c3167f
saimadhavad <smvcaptain@gmail.com>
2024-10-19 10:17:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>2024 Vijayawada Floods</h1>
+        <img src="https://m.economictimes.com/thumb/msid-112972327,width-1600,height-900,resizemode-4,imgsize-148692/vijayawada.jpg" />
+        <p>
+            The recent floods in Vijayawada have brought to light the consequences of encroachment on natural water bodies. 
+            The flood on August 31 in Budameru Rivulet has disrupted life in more than half of Vijayawada. Budameru, a small rivulet 
+            originating in the Khammam district of Telangana, flows through the NTR district before draining into Kolleru Lake in Eluru. 
+            Kolleru Lake, in turn, empties into the Bay of Bengal through Upputeru River, which is the lakes sole outlet channel. 
+            Along its path, excess water from agricultural fields, streams, and water bodies flows into it, making it a vital watercourse. 
+            However, the encroachment along the entire route from Budameru Rivulet to Upputeru is like a continuous chain of unfortunate 
+            intrusions, disrupting the natural flow and flood management. Upputerus proper maintenance is critical for preventing 
+            backflow and managing flood risks, yet it also faces encroachment. Ramanjaneyulu GV of the Centre for Sustainable Agriculture, 
+            Hyderabad, discussed the factors contributing to the floods, during an interview with Usha Peri.
+        </p>
+        <ol>
+          <li>
+          <h2>What were the primary factors that led to the floods in Vijayawada?</h2>
+        <img src="https://munsifdaily.com/wp-content/uploads/2024/09/ANDHRA-RAIN.jpg" alt="" />
+        <p>
+            It is not just one factor at play, but multiple reasons contribute to the situation. There was unusually heavy rainfall, which is not typical for the area. When we look at drains or any infrastructure, they are built based on estimates of how much water they might need to handle or how much rain is expected. However, if the rainfall increases drastically, from 2 cm to 34 cm, no infrastructure can manage that.
+        </p>
+        <p>
+            Another aspect is recognising that what happened in Vijayawada is a natural calamity. This is a direct result of climate change. We have been witnessing climate change impacts over the last five to six years, but we are not addressing it with the seriousness it deserves.
+        </p>
+        <p>
+            Secondly, natural water flow is determined by the lands topography. Over time, there have been many encroachments into these natural waterways, causing blockages. When these blockages occurred, no efforts were made to remove them because such natural calamities were not frequent for the government to address.
+        </p>
+        <p>
+            So, when there was heavy rainfall, the water naturally overflowed. If these encroachments had not existed, perhaps 50 per cent of the rainwater could have been managed. It is important to understand that both the natural calamity and the lack of efforts to stop encroachments are reasons for the flooding in Vijayawada.
+        </p>
+        <p>
+            Thirdly, it is important to consider how accurately and how early the Indian Meteorological Department (IMD) predicts weather patterns. The IMD should provide clear scientific forecasts about whether. While it is understandable that weather can be unpredictable, it raises the question of why private and other weather systems can provide better forecasts, yet the local department is falling short.
+        </p>
+          </li>
+          <li>
+          <h3>How have encroachments contributed to the flooding?</h3>
+        <img src="https://th-i.thgim.com/public/news/national/lf0v8i/article68595770.ece/alternates/BASE_FREE/WhatsApp%20Image%202024-09-02%20at%2012.00.29.jpeg" alt="" />
+        <p>
+            There is no proper approach to reducing the long-term damage caused by climate change. Cities are expanding rapidly, and while many encroachments are unofficial, some are happening through official channels as well. When we encroach on land, we block the natural flow of waterways and alter the water’s path. Instead of focussing all development around one centralised city, we should consider a decentralised approach.
+        </p>
+          </li>
+          <li>
+          <h4>How have urban planning mistakes worsened the effects of climate change in cities?</h4>
+        <img src="https://www.greatandhra.com/newphotos10/vijayawadarains1725722572.jpg" alt="" />
+        <p>
+            Another important point to remember is that heavy rains are mostly affecting urban areas, not rural ones. In climate change, this is known as the “heat island” phenomenon, where urban and peri-urban areas with lots of concrete buildings experience a rise in temperature, creating a vacuum. The changes in climate are not being properly understood, which is a key challenge we face today. Human mistakes like encroachments, poor urban planning, and reshaping landscapes are making things worse. City development will continue, but it must be done responsibly. We are not just encroaching on water bodies but on nature itself.
+        </p>
+          </li>
+          <li>
+          <h5>How should the government address encroachments and silt buildup in the Budameru River to prevent flooding?</h5>
+        <img src="https://telugu.oneindia.com/img/2024/09/boat-prakasam-barrage-042-1725253730.jpg" alt="" />
+        <p>
+            The catchment area of the Budameru Rivulet has also been encroached upon, with encroachments at every step of the water body. The government needs regular planning to understand where water bodies originate, how their flows are affected by urbanisation, and how our land use impacts these flows. 
+        </p>
+        <p>
+            We should consider redefining water flows and, if canals or lakes lack capacity, find ways to increase it, possibly through methods like damming. Dredging is also important because silt that flows into rivers and lakes reduces their storage capacity. For example, Bhavani Island, located upstream of Prakasam Barrage, formed roughly 20 years ago due to silt carried by the water. Silt buildup lowers reservoir capacity, so regular dredging is essential. However, this is not being studied by the government.
+        </p>
+        <p>
+            We tend to react only when calamities occur, instead of understanding the State’s landscape, the origins of water bodies, and their natural flows. In case of heavy rain, we do not know where the water will go, where blockages might occur, or how to clear those pathways. We need both short-term and long-term plans. Short-term plans involve clearing encroachments, dredging, and understanding water bodies and their flows. Long-term plans should focus on assessing changes in human settlements and the shifting climate and building strategies around that.
+        </p>
+          </li>
+        </ol>
+        <h6>Source : The New Indian Express</h6>
+      </header>
+    </div>
+  );
+}
+
+export default App;
